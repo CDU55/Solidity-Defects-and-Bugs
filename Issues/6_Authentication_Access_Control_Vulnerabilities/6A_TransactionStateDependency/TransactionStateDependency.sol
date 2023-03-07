e89619f710b783e5aa33e0b6fd96b7aead1590dc
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2023-03-07 14:50:11 +0200

    used misedCase

--- Issues/6_Authentication_Access_Control_Vulnerabilities/6A_TransactionStateDependency/TransactionStateDependency.sol
+++ Issues/6_Authentication_Access_Control_Vulnerabilities/6A_TransactionStateDependency/TransactionStateDependency.sol
@@ -3,23 +3,23 @@ pragma solidity 0.8.17;
 
 //This contract uses the transaction origin to determine if the caller is the owner of the contract.
 contract TransactionStateDependency {
-    address _owner;
+    address owner;
 
     modifier onlyOwner() {
         require(
-            tx.origin == _owner,
+            tx.origin == owner,
             "Only the owner of the contract can access this"
         );
         _;
     }
 
     constructor() {
-        _owner = msg.sender;
+        owner = msg.sender;
     }
 
     receive() external payable {}
 
     function transferBalance() external onlyOwner {
-        payable(_owner).transfer(address(this).balance);
+        payable(owner).transfer(address(this).balance);
     }
 }
--- Issues/6_Authentication_Access_Control_Vulnerabilities/6A_TransactionStateDependency/TransactionStateDependencyFixed.sol
+++ Issues/6_Authentication_Access_Control_Vulnerabilities/6A_TransactionStateDependency/TransactionStateDependencyFixed.sol
@@ -3,23 +3,23 @@ pragma solidity 0.8.17;
 
 //Fix: Use "msg.sender" instead of "tx.origin".
 contract TransactionStateDependencyFixed {
-    address private _owner;
+    address private owner;
 
     modifier onlyOwner() {
         require(
-            msg.sender == _owner,
+            msg.sender == owner,
             "Only the owner of the contract can access this"
         );
         _;
     }
 
     constructor() {
-        _owner = msg.sender;
+        owner = msg.sender;
     }
 
     receive() external payable {}
 
     function transferBalance() external onlyOwner {
-        payable(_owner).transfer(address(this).balance);
+        payable(owner).transfer(address(this).balance);
     }
 }
